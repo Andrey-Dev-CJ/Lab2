73dc20aae334f51be2a437f597bf049938e54550
Andrey <61851314+Andrey-Dev-CJ@users.noreply.github.com>
2021-12-26 21:51:20 +0300

    Firs part in Lab2
    Был создан класс TRAIN, а также класс для хранения объектов TRAIN и возможности взаимодействия пользователя с объектами класса TRAIN.

---
+++ Menues.cpp
@@ -0,0 +1,62 @@
+#include "Menues.h"
+
+void menuFirst()
+{
+	bool flag = true;
+	int n;
+	Container c;
+	while (flag) {
+		try {
+			std::cout << "-------------Trains menu----------------\n";
+			std::cout << "1. Add trains\n";
+			std::cout << "2. Delete trains\n";
+			std::cout << "3. Change trains\n";
+			std::cout << "4. Show all trains\n";
+			std::cout << "5. Find trains\n";
+			std::cout << "6. Exit\n";
+			std::cout << "It's time to choose, Mr.Freeman: ";
+			cin >> n;
+
+
+			switch (n)
+			{
+			case 1:
+				c.addTr();
+				break;
+			case 2:
+				c.delTr();
+				break;
+			case 3:
+				c.change();
+				break;
+			case 4:
+				c.show();
+				break;
+			case 5:
+				c.find();
+				break;
+
+			default:
+				c.save();
+				flag = false;
+				break;
+			}
+
+
+		}
+		catch (const char* exception) {
+			std::cout << exception;
+
+		}
+		catch (const int exceptions) {
+			if (exceptions == 1)
+				std::cout << "bad index\n";
+		}
+
+	}
+	
+}
+
+void menuSecond()
+{
+}
---
+++ Menues.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+#include "Container.h"
+
+void menuFirst();
+void menuSecond();
